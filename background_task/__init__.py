e062d2f96eb89aa6f127c349d91cfcb9a96b0218
Michael <michamaxe@gmail.com>
2023-01-04 13:03:39 +0100

    fix version

--- background_task/__init__.py
+++ background_task/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-__version__ = '1.2.5'
+__version__ = '1.2.6'
 
 default_app_config = 'background_task.apps.BackgroundTasksAppConfig'
 
+
 def background(*arg, **kw):
-    from background_task.tasks import tasks
-    return tasks.background(*arg, **kw)
+	from background_task.tasks import tasks
+	return tasks.background(*arg, **kw)
